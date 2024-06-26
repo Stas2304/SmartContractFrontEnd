13bf33d3cc04443795765c2314e9ae6d9d7f7341
Stas2304 <ystl1302@gmail.com>
2024-06-27 03:06:33 +0400

    add and connect manifest

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-// this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://Stas2304.github.io/SmartContractFrontEnd/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
